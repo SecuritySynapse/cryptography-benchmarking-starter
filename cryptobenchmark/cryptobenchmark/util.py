8c5935e3de2d4d70b5cb4ab72062513bb5b1b0bd
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-09-23 14:19:08 -0400

    chore: Add the starting version of the cryptobenchmark/cryptobenchmark/util.py file.

---
+++ cryptobenchmark/cryptobenchmark/util.py
@@ -0,0 +1,36 @@
+"""Utilities for performance evaluation of cryptography."""
+
+from cryptography.fernet import Fernet
+from rfernet import Fernet as rFernet  # type: ignore
+
+# Data generation function {{{
+
+# TODO: Add a complete implementation of a function
+# that generates random text of a specified size
+#
+
+# }}}
+
+# Benchmarking functions {{{
+
+# TODO: Add a complete implementation of
+# functions that perform benchmarking according
+# to the requirements in the README.md file
+
+# }}}
+
+# Encryption functions {{{
+
+# TODO: Provide an implementation of the two
+# encryption functions for the Fernet and Rust-based
+# Fernet implementations
+
+# }}}
+
+# Decryption functions {{{
+
+# TODO: Provide an implementation of the two
+# decryption functions for the Fernet and Rust-based
+# Fernet implementations
+
+# }}}
